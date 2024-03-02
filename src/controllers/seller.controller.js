2fdf9dfa5256bcfe024b6210ff5e9b62afd9e203
Pramoth Dilshan <pramothdilshan2001@gmail.com>
2024-03-02 14:58:43 +0530

    feat: do some changes in the database

--- src/controllers/seller.controller.js
+++ src/controllers/seller.controller.js
@@ -2,7 +2,7 @@ const sellerService = require("../services/seller.service");
 
 async function createSeller(req, res) {
   const seller = await sellerService.createSeller(req.body);
-  res.json(seller);
+  res.json(seller).status(201);
 }
 
 async function getAllSellers(req, res) {
