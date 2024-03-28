31b82dee7c72faec36f4eb7181083867116dbc64
charukabandara22 <144276388+charukabandara22@users.noreply.github.com>
2024-03-29 01:02:27 +0530

    update

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -4,7 +4,9 @@ const orderController = require("../controllers/order.controller");
 
 //set the page name
 
-//router.post("/order/addOrders", orderController.createOrders);
+router.post("/order/addOrders", orderController.createOrders);
 router.get("/orders", orderController.getAllOrders);
+
+//router.post("/orders/changeStatus", productController.changeStatus);
 
 module.exports = router;
--- src/services/order.service.js
+++ src/services/order.service.js
@@ -2,29 +2,71 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function getAllOrders() {
-  const ordersWithCustomer = await prisma.order.findMany({
+  const ordersWithCustomer = await prisma.order.findMany(
+    {
     include: {
       customer: {
         select: {
           firstName: true,
           lastName: true,
           contactNumber: true,
+          orders: true,
+         },
+       },
+       product: { 
+        select: {
+          productCode: true,
+          name: true,
+          price: true,
         },
-      },
-    },
-  });
+       },
+     },
+    });
 
   // Transforming the data to include the customerName
   const orders = ordersWithCustomer.map(order => ({
     ...order,
     customerName: `${order.customer.firstName} ${order.customer.lastName}`,
     contactNumber: `${order.customer.contactNumber}`,
+    allOrder: `${order.customer.orders}`,
+    productCode: `${order.product.productCode}`,
+    unitPrice: `${order.product.price}`,
+    // productDescription: `${order.product.description}`,   
   }));
 
   return orders;
 }
 
+// this is additional
+const createOrders = async () => {
+  return await prisma.order.update({
+    where: {
+      orderId: 1,
+    },
+    data: {
+      orderStatus: "success" ,
+    },
+  });
+}
+
+
+// async function createOrders(orderStatus) {
+//   try {
+//     const newData = await prisma.data.create({
+//       data: {
+//         orderStatus,
+//       },
+//     });
+//     return { success: true, data: newData };
+//   } catch (error) {
+//     return { success: false, error: 'Error storing data' };
+//   }
+// }
+
+
+
 
 module.exports = {
   getAllOrders,
+  createOrders
 };
