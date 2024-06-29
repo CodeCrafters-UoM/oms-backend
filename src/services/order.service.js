c8a6e634e14aced386975c15a03b854f119ad211
Pramoth Dilshan <pramothdilshan2001@gmail.com>
2024-06-29 22:36:39 +0530

    fix: remove some column in the order table

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -5,55 +5,62 @@ const prisma = new PrismaClient();
 
 async function getAllOrders(id) {
   try {
-  const ordersWithCustomer = await prisma.order.findMany({
-    where: {
-      sellerId: id,
-    },
-    include: {
-      customer: {
-        select: {
-          id: true,
-          firstName: true,
-          lastName: true,
-          contactNumber: true,
-          orders: true, 
+    const ordersWithCustomer = await prisma.order.findMany({
+      where: {
+        sellerId: id,
+      },
+      include: {
+        customer: {
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+            contactNumber: true,
+            orders: true,
+          },
+        },
+        product: {
+          select: {
+            productCode: true,
+            name: true,
+            price: true,
+            description: true,
+          },
         },
       },
-      product: {
-        select: {
-          productCode: true,
-          name: true,
-          price: true,
-          description: true,
-        },
-      },
-    },
-  });
+    });
 
-  const orders = ordersWithCustomer.map((order) => {
-    const customerOrders = order.customer.orders;
-    const customerOrdersForSeller = customerOrders.filter(o => o.sellerId === id);
-    const customerReturnOrders = customerOrders.filter(o => o.orderStatus === 'RETURN');
-    const customerReturnOrdersForSeller = customerReturnOrders.filter(o => o.sellerId === id);
+    const orders = ordersWithCustomer.map((order) => {
+      const customerOrders = order.customer.orders;
+      const customerOrdersForSeller = customerOrders.filter(
+        (o) => o.sellerId === id
+      );
+      const customerReturnOrders = customerOrders.filter(
+        (o) => o.orderStatus === "RETURN"
+      );
+      const customerReturnOrdersForSeller = customerReturnOrders.filter(
+        (o) => o.sellerId === id
+      );
 
-    return {
-      ...order,
-      customerName: `${order.customer.firstName} ${order.customer.lastName}`,
-      contactNumber: order.customer.contactNumber,
-      productCode: order.product.productCode,
-      unitPrice: order.product.price,
-      description: order.product.description,
-      totalOrdersForCustomer: customerOrders.length,
-      totalOrdersForCustomerForSeller: customerOrdersForSeller.length,
-      totalReturnOrdersForCustomer: customerReturnOrders.length,
-      totalReturnOrdersForCustomerForSeller: customerReturnOrdersForSeller.length,
-    };
-  });
+      return {
+        ...order,
+        customerName: `${order.customer.firstName} ${order.customer.lastName}`,
+        contactNumber: order.customer.contactNumber,
+        productCode: order.product.productCode,
+        unitPrice: order.product.price,
+        description: order.product.description,
+        totalOrdersForCustomer: customerOrders.length,
+        totalOrdersForCustomerForSeller: customerOrdersForSeller.length,
+        totalReturnOrdersForCustomer: customerReturnOrders.length,
+        totalReturnOrdersForCustomerForSeller:
+          customerReturnOrdersForSeller.length,
+      };
+    });
 
-  return orders;
-} catch (error) {
-  throw new Error(`Error creating order and customer: ${error}`);
-}
+    return orders;
+  } catch (error) {
+    throw new Error(`Error creating order and customer: ${error}`);
+  }
 }
 
 const updateStatus = async (id, status) => {
@@ -92,9 +99,6 @@ const createOrder = async (order) => {
         deliveryAddress: order["Your delivery address : "],
         paymentMethod: order["Your preferred payment method : "],
         orderStatus: "NEW",
-        size: order["Size of the item : "],
-        color: order["Color of the item : "],
-        comments: order["Comments : "],
         sellerId: order.seller,
         customerId: order["Your phone number : "],
         customisedAnswers: order.customisedAnswers,
